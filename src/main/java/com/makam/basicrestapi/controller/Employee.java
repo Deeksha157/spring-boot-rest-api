71919109d702f7ff6b44850303382dedd411b550
Deeksha <deekshamakam47@gmail.com>
2023-06-09 15:43:00 +0200

    removed the import statements- arrays and logging level

--- src/main/java/com/makam/basicrestapi/controller/Employee.java
+++ src/main/java/com/makam/basicrestapi/controller/Employee.java
@@ -2,13 +2,9 @@ package com.makam.basicrestapi.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.logging.Level;
 
 @RestController
 public class Employee {
