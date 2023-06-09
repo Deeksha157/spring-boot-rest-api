ea5ce4056fd15234589cda7978cc402533d698d8
Deeksha <deekshamakam47@gmail.com>
2023-06-09 15:40:10 +0200

    changed the annotation to RestController which doesn't need a requestBody

--- src/main/java/com/makam/basicrestapi/controller/Employee.java
+++ src/main/java/com/makam/basicrestapi/controller/Employee.java
@@ -5,18 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.logging.Level;
 
-@Controller
+@RestController
 public class Employee {
 
     private static final Logger logger = LoggerFactory.getLogger(Employee.class);
 
     @GetMapping("/basic-get")
-    @ResponseBody
+
     public String basicApi(@RequestParam int id, @RequestParam String name, @RequestParam String role, @RequestParam String location) {
         logger.info("Request parameters: id= " + id + "; name= "+ name + "; role= " + role +"; location= "+ location);
         return "success";
